c102a737ea757c264d8f449c1017822ef6719316
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-01-17 11:20:02 -0800

    `Store` methods put into prototype

--- js/app.js
+++ js/app.js
@@ -7,7 +7,53 @@ function generateRandomNumberOfCustomers (min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
-const seattle = {
+function Store(location, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerCustomer) {
+    this.location = location;
+    this.minHourlyCustomers = minHourlyCustomers;
+    this.maxHourlyCustomers = maxHourlyCustomers;
+    this.avgCookiesPerCustomer = avgCookiesPerCustomer;
+    this.customersPerHour = [];
+    this.cookiesPerHour = [];
+    this.totalCookiesSold = 0;
+    this.salesReport = [];
+}
+
+Store.prototype.generateCustomersPerHour = function () {
+        
+    for (let i = 0; i < 14; i++) {
+        this.customersPerHour.push(Math.round(generateRandomNumberOfCustomers(this.minHourlyCustomers, this.maxHourlyCustomers))); 
+    }
+    //console.log('Customers ' + this.customersPerHour);
+};
+
+Store.prototype.generateCookiesPerHour = function () {
+
+    for (let i = 0; i < 14; i++) {
+        this.cookiesPerHour.push(Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer)); 
+    }
+    //console.log('Cookies ' + this.cookiesPerHour);
+};
+
+Store.prototype.calculateTotalCookiesSold = function () {
+    let grandTotal = 0;
+    this.cookiesPerHour.forEach(total => grandTotal += total );
+    this.totalCookiesSold = grandTotal;
+};
+
+Store.prototype.generateSalesReport = function () {
+    for(let i = 0; i < 14; i++) {
+        this.salesReport.push([/*${hours[i]}:*/`${this.cookiesPerHour[i]} Cookies`])
+    };
+    this.salesReport.push(`${this.totalCookiesSold} Cookies`);
+};
+
+let seattle = new Store('Seattle', 23, 65, 6.3);
+let tokyo = new Store('Tokyo', 3, 24, 1.2);
+let dubai = new Store('Dubai', 11, 38, 3.7);
+let paris = new Store('Paris', 20, 38, 2.3)
+let lima = new Store('Lima', 2, 16, 4.6);
+
+/*const seattle = {
     location: 'Seattle',
     minHourlyCustomers: 23,
     maxHourlyCustomers: 65,
@@ -41,7 +87,7 @@ const seattle = {
 
     generateSalesReport: function () {
         for(let i = 0; i < 14; i++) {
-            this.salesReport.push([/*${hours[i]}:*/`${this.cookiesPerHour[i]} Cookies`])
+            this.salesReport.push([`${this.cookiesPerHour[i]} Cookies`])
         };
         this.salesReport.push(`${this.totalCookiesSold} Cookies`);
     },
@@ -205,7 +251,7 @@ const lima = {
         };
         this.salesReport.push(`${this.totalCookiesSold} Cookies`);
     },
-}
+}*/
 
 const locations = [seattle, tokyo, dubai, paris, lima];
 
